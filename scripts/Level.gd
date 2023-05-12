0361b9aa3e12578eea7cf0bf7f1a076602b833c8
Qualpo <toxixbeluga@gmail.com>
2023-05-11 21:51:46 -0700

    wrgwgd

--- scripts/Level.gd
+++ scripts/Level.gd
@@ -3,9 +3,9 @@ extends Node3D
 
 @export var BGM : AudioStream 
 # Called when the node enters the scene tree for the first time.
+
 func _ready():
 	Music.PlaySong(BGM)
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
---
+++ scripts/LoadingScreen.gd
@@ -0,0 +1,19 @@
+extends Control
+@export var scene:String = "res://scenes/levels/Level1.tscn"
+var Progress = []
+var SceneLoadStatus = 0
+var doing = false
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	scene = Global.TargetScenePath
+	ResourceLoader.load_threaded_request(scene)
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	SceneLoadStatus = ResourceLoader.load_threaded_get_status(scene,Progress)
+	$ProgressBar.value = Progress[0] * 100
+	if SceneLoadStatus == ResourceLoader.THREAD_LOAD_LOADED:
+		if not doing:
+			doing = true
+			get_tree().change_scene_to_packed(ResourceLoader.load_threaded_get(scene))
